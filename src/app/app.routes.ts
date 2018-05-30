c76cee94e8b15ac535549da43b214bf1f1525e20
Alecsander Marques <alecsander_marques@hotmail.com>
2018-05-30 12:35:50 -0300

    Lazy-loading component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
-import {Routes} from '@angular/router'
+import { Routes } from '@angular/router'
 
-import {HomeComponent} from './home/home.component'
-import {AboutComponent} from './about/about.component'
+import { HomeComponent } from './home/home.component'
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
@@ -10,16 +9,18 @@ import { OrderComponent } from './order/order.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 export const ROUTES: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'restaurants', component: RestaurantsComponent},
-    {path: 'restaurants/:id', component: RestaurantDetailComponent,
+    { path: '', component: HomeComponent },
+    { path: 'restaurants', component: RestaurantsComponent },
+    {
+        path: 'restaurants/:id', component: RestaurantDetailComponent,
         children: [
-            {path: '', redirectTo: 'menu', pathMatch: 'full'},
-            {path: 'menu', component: MenuComponent},
-            {path: 'reviews', component: ReviewsComponent}
-        ]},
-    {path: 'order', component: OrderComponent},
-    {path: 'order-summary', component: OrderSummaryComponent},
-    {path: 'about', component: AboutComponent}
-    
+            { path: '', redirectTo: 'menu', pathMatch: 'full' },
+            { path: 'menu', component: MenuComponent },
+            { path: 'reviews', component: ReviewsComponent }
+        ]
+    },
+    { path: 'order', component: OrderComponent },
+    { path: 'order-summary', component: OrderSummaryComponent },
+    { path: 'about', loadChildren: './about/about.module#AboutModule' }
+
 ]
