4de075907a0e59a0c1b0a9d9f28e6d09dc1b45ed
Alecsander Marques <alecsander_marques@hotmail.com>
2018-06-26 10:29:36 -0300

    apply preload all modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from "@angular/core";
+import { NgModule, ModuleWithProviders } from "@angular/core";
 import { InputComponent } from "./input/input.component";
 import { RadioComponent } from "./radio/radio.component";
 import { RatingComponent } from "./rating/rating.component";
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
+import { RestaurantsService } from "../restaurants/restaurants.service";
+import { OrderService } from "../order/order.service";
 
 @NgModule({
   declarations: [InputComponent, RadioComponent, RatingComponent],
@@ -12,4 +15,11 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
             CommonModule, FormsModule, ReactiveFormsModule]
 })
 
-export class SharedModule {}
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [ShoppingCartService, RestaurantsService, OrderService]
+    }
+  }
+}
